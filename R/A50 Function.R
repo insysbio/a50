56071b041cb4c277d0782f8d9505ee265dd44aa6
stepanov <stepanov@insysbio.com>
2024-01-25 17:07:02 +0300

    update

--- R/A50 Function.R
+++ R/A50 Function.R
@@ -1,20 +1,18 @@
 #
 #    A50 Calculator
 #
-
-## First specify the packages of interest
-packages = c("stringr", "dplyr", "tibble")
+# packages = c("stringr", "dplyr", "tibble")
 
 ## Now load or install&load all
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
+# package.check <- lapply(
+#   packages,
+#   FUN = function(x) {
+#     if (!require(x, character.only = TRUE)) {
+#       install.packages(x, dependencies = TRUE)
+#       library(x, character.only = TRUE)
+#     }
+#   }
+# )
 
 extract <- function(text) {
     text |>
@@ -41,5 +39,3 @@ A50 <- function(x, y) {
     unique() |>
     max() ## |> round(3)
 }
-
-## A50(x,y)
